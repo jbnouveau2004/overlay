b0c0a1f8efeae3a3c32453e493a17f3ac6509beb
stagiaire ONLINEFORMAPRO <stagiaire2022@onlineformapro.org>
2023-09-18 14:46:05 +0200

    version javascript

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
+let overlay = document.querySelector(".overlay");
+let ouverture = document.querySelector(".ouverture");
+let fermeture = document.querySelector(".fermeture");
+let calque = document.querySelector(".calque");
 
 
+document.body.addEventListener("mouseup", test);
+
+function test(event){
+    let test = getComputedStyle(overlay).getPropertyValue("display");
+
+    if (test == 'block' && (!overlay.contains(event.target) || (fermeture.contains(event.target)))){
+        overlay.style.display = 'none';
+        calque.style.display = 'none';
+
+    }else if(test == 'none' && ouverture.contains(event.target)){
+        overlay.style.display = 'block';
+        calque.style.display = 'block';
+    }
+
+}
